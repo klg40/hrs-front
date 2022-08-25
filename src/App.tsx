e51e6edd6bdc48903c0aa4f98b9abd62c6ad19d9
Artem <zausaylov.ao@gmail.com>
2022-08-26 01:47:34 +0300

    install socket.io-client and create first connection

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import { io } from 'socket.io-client';
+
+const socket = io('ws://localhost:4000');
 
 function App() {
+
+    useEffect(() => {
+        socket.emit('mount')
+    }, [])
+
+    const handleClick = () => {
+        socket.emit('ping', Date.now())
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <button onClick={handleClick}>Click</button>
   );
 }
 
